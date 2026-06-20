ecdbbbdd0856f4cb9abdc2bda900015d12b03de4
Vincent Brochard <vincentbrochard.ac@gmail.com>
2026-06-20 19:23:32 +0200

    CLI File with Display print
    * Move print function
    
    * impl display for print
    
    ---------
    
    Co-authored-by: VBrochard <Vincent@DESKTOP-AEGVBHR.localdomain>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod cli;
+
 use lyrem::elf::parser::parse_elf;
 use std::env;
 use std::path::Path;
@@ -5,28 +7,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let chemin = Path::new(&query);
-    let response = parse_elf(chemin);
-    match response {
-        Ok(metadata) => {
-            println!("File Information");
-            println!("----------------");
-            println!("Name         : {}", metadata.file.name);
-            println!("Path         : {}", metadata.file.path);
-            println!("Size         : {} bytes", metadata.file.size);
-
-            println!();
-            println!("ELF Header");
-            println!("----------");
-            println!("Class        : {:?}", metadata.header.class);
-            println!("Endianess    : {:?}", metadata.header.endianess);
-            println!("ABI          : {:?}", metadata.header.abi);
-            println!("Architecture : {:?}", metadata.header.architecture);
-            println!("Type         : {:?}", metadata.header.binary_type);
-            println!("Entry Point  : 0x{:X}", metadata.header.entry_point);
-        }
-
-        Err(error) => {
-            println!("Error: {:?}", error);
-        }
+    match parse_elf(chemin) {
+        Ok(metadata) => cli::print_metadata(&metadata),
+        Err(error) => cli::print_error(&error),
     }
 }
