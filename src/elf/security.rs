f2f6a4fd25b60168935b8babab1582d142e12eab
VBrochard <Drahcorb@proton.me>
2026-07-10 23:25:51 +0200

    Begin Security Analysis and refactoring main

---
+++ src/elf/security.rs
@@ -0,0 +1,89 @@
+use core::fmt;
+
+use crate::elf::{
+    metadata::ElfMetadata,
+    program::{ProgramHeader, ProgramType},
+    security::Status::{Disabled, Enabled},
+};
+
+#[derive(Debug, PartialEq)]
+pub enum Status {
+    Enabled,
+    Disabled,
+    Unknown,
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Status::Enabled => write!(f, "Enabled"),
+            Status::Disabled => write!(f, "Disabled"),
+            Status::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum RelroStatus {
+    None,
+    Present,
+    Unknown,
+}
+
+impl fmt::Display for RelroStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RelroStatus::None => write!(f, "None"),
+            RelroStatus::Present => write!(f, "Present"),
+            RelroStatus::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DynamicLinkingStatus {
+    Dynamic,
+    Static,
+    Unknown,
+}
+
+impl fmt::Display for DynamicLinkingStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DynamicLinkingStatus::Dynamic => write!(f, "Dynamic"),
+            DynamicLinkingStatus::Static => write!(f, "Static"),
+            DynamicLinkingStatus::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct SecurityAnalysis {
+    pub nx: Status,
+    pub pie: Status,
+    pub relro: RelroStatus,
+    pub rwx_segment: Vec<ProgramHeader>,
+    pub has_dynamic_segment: bool,
+    pub has_interpreter: bool,
+}
+
+pub fn analyze(metadata: &ElfMetadata) -> SecurityAnalysis {
+    let mut response = SecurityAnalysis {
+        nx: Status::Unknown,
+        pie: Status::Unknown,
+        relro: RelroStatus::Unknown,
+        rwx_segment: Vec::new(),
+        has_dynamic_segment: false,
+        has_interpreter: false,
+    };
+    for program_header in &metadata.prog_header {
+        if program_header.program_type == ProgramType::GnuStack {
+            if program_header.flags.executable {
+                response.nx = Disabled
+            } else {
+                response.nx = Enabled
+            }
+        }
+    }
+    response
+}
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ mod cli;
 use crate::cli::print_error;
 use crate::cli::print_metadata;
 use crate::cli::print_program_headers;
+use crate::cli::print_security_analysis;
 use clap::Parser;
 use lyrem::elf::parser::parse_elf;
+use lyrem::elf::security::analyze;
 use std::path::Path;
 use std::path::PathBuf;
 
@@ -18,28 +20,19 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     let chemin = Path::new(&cli.path);
-    if cli.json {
-        match parse_elf(chemin) {
-            Ok(metadata) => {
-                if cli.json {
-                    match serde_json::to_string_pretty(&metadata) {
-                        Ok(json) => println!("{json}"),
-                        Err(error) => eprintln!("JSON error: {error}"),
-                    }
-                } else {
-                    print_metadata(&metadata);
-                    print_program_headers(&metadata);
+    match parse_elf(chemin) {
+        Ok(metadata) => {
+            if cli.json {
+                match serde_json::to_string_pretty(&metadata) {
+                    Ok(json) => println!("{json}"),
+                    Err(error) => eprintln!("JSON error: {error}"),
                 }
-            }
-            Err(error) => print_error(&error),
-        }
-    } else {
-        match parse_elf(chemin) {
-            Ok(metadata) => {
+            } else {
                 print_metadata(&metadata);
                 print_program_headers(&metadata);
+                print_security_analysis(&analyze(&metadata));
             }
-            Err(error) => print_error(&error),
         }
+        Err(error) => print_error(&error),
     }
 }
